69e1ed2ef3d553f88a46d64828b97568279b52e5
Daniil <105662394+Daniil10295@users.noreply.github.com>
2026-06-22 14:09:54 +0300

    Move 2 versions into 1 branch, need fix for assets folder (not imported)

---
+++ mc1_21_11/src/main/java/dev/lutsu/hidehotbar/HideHotbarModClient.java
@@ -0,0 +1,81 @@
+package dev.lutsu.hidehotbar;
+
+import dev.lutsu.hidehotbar.config.ToolBarConfig;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class HideHotbarModClient implements ClientModInitializer {
+    public KeyBinding toggleHudKeyBinding;
+
+    @Override
+    public void onInitializeClient() {
+        toggleHudKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.hidehotbar.toggle_hud",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F7,
+                KeyBinding.Category.MISC
+//                "key.categories.hidehotbar"
+        ));
+
+        if (ToolBarConfig.unhide_on_restart) {
+            ToolBarConfig.hid = false;
+        }
+        ClientTickEvents.END_CLIENT_TICK.register(this::clientTickListener);
+    }
+
+    protected void clientTickListener(MinecraftClient client){
+        if (!ToolBarConfig.enabled) { // skip button press when disabled
+            DisableHotbarAlert(client);
+            return;
+        }
+
+        while (toggleHudKeyBinding.wasPressed()) {
+            toggleHotbar(client);
+        }
+
+        if (client.player != null){
+            float hpPercentage = client.player.getHealth() / client.player.getMaxHealth() * 100;
+
+            if (hpPercentage < ToolBarConfig.low_hp_percentage
+                    && ToolBarConfig.unhide_on_low_hp
+                    && ToolBarConfig.hid) {
+                toggleHotbar(client);
+            }
+
+            var foodManager = client.player.getHungerManager();
+            float hungerPercentage = (float) foodManager.getFoodLevel() / 20 *100;
+            if (hungerPercentage < ToolBarConfig.low_food_percentage
+                    && ToolBarConfig.unhide_on_low_food
+                    && ToolBarConfig.hid) {
+                toggleHotbar(client);
+            }
+        }
+    }
+
+    public static void toggleHotbar(MinecraftClient client) {
+        ToolBarConfig.hid = !ToolBarConfig.hid;
+        SendHotbarAlert(client);
+    }
+
+    protected static void SendHotbarAlert(MinecraftClient client) {
+        if (client.player != null) {
+            client.player.sendMessage(Text.translatable(
+                    ToolBarConfig.hid
+                            ? "hidehotbar.hud_hidden"
+                            : "hidehotbar.hud_shown"
+            ), true);
+        }
+    }
+
+    protected void DisableHotbarAlert(MinecraftClient client) {
+        if (client.player != null) {
+            client.player.sendMessage(Text.translatable("hidehotbar.disabled"), true);
+        }
+    }
+}
