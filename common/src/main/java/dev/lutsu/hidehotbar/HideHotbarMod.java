4618803f2d906768df13253f84853e44521025c2
Daniil <105662394+Daniil10295@users.noreply.github.com>
2026-06-22 16:05:23 +0300

    version number update, workflow upd

--- common/src/main/java/dev/lutsu/hidehotbar/HideHotbarMod.java
+++ common/src/main/java/dev/lutsu/hidehotbar/HideHotbarMod.java
@@ -4,8 +4,6 @@ import dev.lutsu.hidehotbar.config.ToolBarConfig;
 import net.fabricmc.api.ModInitializer;
 
 public class HideHotbarMod implements ModInitializer {
-    public static final String MOD_ID = "hidehotbar";
-
     @Override
     public void onInitialize() {
         ToolBarConfig.load();
